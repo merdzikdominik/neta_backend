795f66f58fea12130e79dec52b1a9df8d762f9a8
Dominik <dominik.merdzik@onet.pl>
2024-03-04 14:04:23 +0100

    able to delete notifications from db

--- api/views.py
+++ api/views.py
@@ -324,3 +324,7 @@ class NotificationView(APIView):
         serializer = NotificationSerializer(all_notifications, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, format=None):
+        Notification.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
